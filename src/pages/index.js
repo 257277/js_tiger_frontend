9d158f012a91d0ca2dda8c35df6d4751d4a46e54
Rahul Sharma <40212890+257277@users.noreply.github.com>
2023-06-17 00:18:46 +0530

    up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,22 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { useRouter } from 'next/router';
-
+import { useState, useEffect } from "react";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter();
+  useEffect(() => {
+    router.push('/login');
 
+  }, [])
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div>
-
       </div>
 
     </main>
   )
 }
+
